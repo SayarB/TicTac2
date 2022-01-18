81a0b2c9ac58ae2fc944e92ca8e517b57202400d
SayarB <sayarbhattacharyya03@gmail.com>
2022-01-18 13:56:14 +0530

    updated ui and matchmaking

--- components/Game/Game.js
+++ components/Game/Game.js
@@ -22,7 +22,11 @@ export default function Game({ username, code, method }) {
 
   useEffect(() => {
     if (socket == null) return;
-    if (username == null || roomid == null) return;
+    if (username == null || roomid === null) {
+      console.log("roomid not found");
+      router.back();
+      return;
+    }
     if (method === "create") socket.emit("create-room", username);
     else if (method === "join") socket.emit("join-room", username, code);
 
@@ -31,6 +35,12 @@ export default function Game({ username, code, method }) {
     });
     socket.on("joined-room", (room) => {
       setRoomId(room);
+    });
+    socket.on("room_full", () => {
+      router.back();
+    });
+    socket.on("roomdoesnotexist", () => {
+      router.back();
     });
     socket.on("buttonClicked", () => {
       alert("button clicked by other user");
@@ -42,21 +52,21 @@ export default function Game({ username, code, method }) {
     socket.on("set-turn", (turnToBeAssigned) => {
       setTurn(turnToBeAssigned);
     });
-    socket.on("set-draw", (draw) => {
+    socket.on("set_draw", (draw) => {
+      console.log("draw");
       setIsDraw(true);
       setGameOver(true);
     });
-    socket?.on("update_game_state", (g_state) => {
+    socket.on("update_game_state", (g_state) => {
       setGame_state(g_state);
       console.log(game_state);
     });
-    socket?.on("set_winner", ({ isWin, comb, winner_turn }) => {
+    socket.on("set_winner", ({ isWin, comb, winner_turn }) => {
+      console.log("winner_turn = ", winner_turn);
       if (isWin) {
         setWinnerTurn(winner_turn);
-        if (turn === winner_turn) setWinner(username);
-        else setWinner(opponent);
-        setGameOver(true);
         showWinComb(comb);
+        setGameOver(true);
       }
     });
     socket.on("set_opponent", (opponent) => setOpponent(opponent));
@@ -65,7 +75,7 @@ export default function Game({ username, code, method }) {
     socket?.emit("put_turn", roomid, pos, turn);
   };
   const showWinComb = (winComb) => {
-    console.log(winComb);
+    console.log("showing combination :", winnerTurn === turn ? true : false);
     winComb.forEach((ele) => {
       const cell = document.getElementById("cell-" + ele);
       cell.classList.add(styles.winner_combination);
@@ -78,15 +88,30 @@ export default function Game({ username, code, method }) {
     }
   }, [turn]);
 
+  useEffect(() => {
+    if (winnerTurn > -1) {
+      if (turn === winnerTurn) setWinner(username);
+      else setWinner(opponent);
+    }
+  }, [winnerTurn]);
+
   if (game_state.length === 9) {
     return (
       <div className={styles.game_container}>
-        Turn = {turn} {username} {opponent}
+        <h1>You are {turn === 0 ? "O" : "X"}</h1>
+        <h1>{username}</h1>
+        <h1>{opponent}</h1>
         <Board game_state={game_state} putTurn={emitTurn} />
         {winnerTurn !== -1 ? (
           <div className={styles.overlay}>
-            <h1 className={styles.announcement + " " + styles.winner_text}>
-              Winner is Player {winner}
+            <h1
+              className={
+                styles.announcement +
+                " " +
+                (winnerTurn === turn ? styles.winner_text : styles.loser_text)
+              }
+            >
+              {winnerTurn !== turn ? `You Lost to ${winner}` : `You won`}
             </h1>
           </div>
         ) : (
